91968403e9fbd1aa05069eaf2b1321b9cbec45cb
NafisNoorNabil <nafisnoornabil29@gmail.com>
2024-04-16 11:09:44 +0600

    Practice Vue JS

---
+++ Random Person Generator/app.js
@@ -0,0 +1,22 @@
+const app=Vue.createApp({
+    data(){
+        return {
+            name: 'Nabil',
+            email:"nafisnoor@gmail.com",
+
+
+        }
+    },
+    methods:{
+        async getUser(){
+            const res=await fetch('https://randomuser.me/api')
+            const {results} = await res.json()
+            this.firstName=results[0].name.first
+            this.lastName=results[0].name.last
+            this.email=results[0].email
+            this.gender=results[0].gender
+            this.picture=results[0].picture.large
+        }
+    }
+})
+app.mount("#app")
